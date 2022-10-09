50939de8239ce87fdffe4736efab91d54fd5971d
Huy Phan <duc.phan@kcl.ac.uk>
2022-10-09 18:54:43 +0100

    Do Exercise 2

--- primes.py
+++ primes.py
@@ -2,8 +2,8 @@
 """ENTER YOUR SOLUTION HERE!"""
 
 def isPrime(number):
-    for i in range(2, int(n+2)+1):
-        if (n % i) == 0:
+    for i in range(2, int(number+2)+1):
+        if (number % i) == 0:
             return False
         else:
             return True
